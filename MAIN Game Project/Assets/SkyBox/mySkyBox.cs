a0918027f6bc913b8c8681cdff31e0d79b2daf94
ErikVeld <e.p.veldhuis@gmail.com>
2014-12-03 17:12:17 +0100

    skybox Added

---
+++ MAIN Game Project/Assets/SkyBox/mySkyBox.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// My sky box >>> Unity skybox;
+/// Author: Erik Veldhuis
+/// </summary>
+public class mySkyBox : MonoBehaviour {
+
+	public Vector3 skyBoxCenter;
+	public float skyBoxSize; 			// square box for convinience, this value is no visual box
+	public float skyBoxScaleFac = 1f;
+
+	private Vector3 worldBoxCenter = new Vector3(0, 0, 0); //always origin
+
+	private GameObject skyCam;
+	private Camera skyboxCamera;
+
+	public LayerMask cullingMask;
+	private Camera mainCamera;
+
+	private Transform skyDomeOrBox;
+
+
+
+	// Use this for initialization
+	void Start () {
+		// find main camera
+		mainCamera = Camera.main;//GameObject.FindGameObjectWithTag("MainCamera");
+
+		skyCam = new GameObject();
+		skyboxCamera = skyCam.AddComponent<Camera>() as Camera;
+		skyCam.name = "mySkyBoxCamera";
+
+		skyboxCamera.depth = -10;
+		skyboxCamera.clearFlags = CameraClearFlags.Skybox;
+		skyboxCamera.orthographic = false;
+		skyboxCamera.cullingMask = cullingMask;
+		skyboxCamera.transform.position = skyBoxCenter;
+		skyboxCamera.nearClipPlane = 0.001f;
+		skyboxCamera.farClipPlane = 3f * skyBoxSize;
+
+		//camCam.cu
+
+		//cam.transform.position = new Vector3(1,1,1);
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (Camera.main != null){
+			skyboxCamera.transform.rotation = Camera.main.transform.rotation;
+			skyboxCamera.transform.position = skyBoxCenter + Camera.main.transform.position/skyBoxSize * skyBoxScaleFac;
+
+			skyboxCamera.fieldOfView = Camera.main.fieldOfView;
+		}
+	}
+}
